2f525b6c147edabb5463e4ef10738284cd0b71cc
youRock19 <yyyuras191@mail.ru>
2022-05-16 20:09:58 +0300

    version 1.0

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -2,8 +2,15 @@ package koschei.models;
 
 public class Egg6 {
 
+    private Needle7 neddle;
+
+    public Egg6(Needle7 neddle) {
+        this.neddle = neddle;
+    }
+
+
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + neddle.toString();
     }
 }
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -2,6 +2,13 @@ package koschei.models;
 
 public class Needle7 {
 
+
+    private Deth8 deth;
+
+    public Needle7(Deth8 deth) {
+        this.deth = deth;
+    }
+
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
